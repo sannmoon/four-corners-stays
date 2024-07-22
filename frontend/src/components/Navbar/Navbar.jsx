9090e5bd481979b72c5d14506617f023e4859c00
Sansan <msandartun@gmail.com>
2024-07-22 19:11:34 +0800

    mobile screen fixes

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,15 @@
 import "./Navbar.scss";
 import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/logo.png";
+import menu from "../../assets/menu.png";
 import LanguageSelector from "../LanguageSelector/LanguageSelector";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 function Navbar() {
   const { t } = useTranslation();
+  const [open, setOpen] = useState(false);
+
   const location = useLocation();
 
   const [navbar, setNavbar] = useState(false);
@@ -19,20 +22,33 @@ function Navbar() {
     }
   };
 
+  const navLinks = (
+    <>
+      <Link to="/">{t("nav_home")}</Link>
+      <Link to="/rooms">{t("nav_rooms")}</Link>
+      <Link to="/attractions">{t("nav_attractions")}</Link>
+      <Link to="/about"> {t("nav_about")} </Link>
+      <Link to="/contact"> {t("nav_contact")} </Link>
+    </>
+  );
+
   window.addEventListener("scroll", changeBackground);
   const isHomepage = location.pathname === "/";
   return (
     <div className={navbar || !isHomepage ? "navbar active" : "navbar"}>
       <img src={logo} alt="logo" />
       <div className="navRight">
-        <div className="navLinks">
-          <Link to="/">{t("nav_home")}</Link>
-          <Link to="/rooms">{t("nav_rooms")}</Link>
-          <Link to="/attractions">{t("nav_attractions")}</Link>
-          <Link to="/about"> {t("nav_about")} </Link>
-          <Link to="/contact"> {t("nav_contact")} </Link>
+        <div className="navLinks">{navLinks}</div>
+        <div className="langSelector">
+          <LanguageSelector />
         </div>
-        <LanguageSelector />
+        <div className="menuIcon">
+          <img src={menu} alt="" onClick={() => setOpen((prev) => !prev)} />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <LanguageSelector />
+          {navLinks}
+        </div>
       </div>
     </div>
   );
