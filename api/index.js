e71e08d059cf4879ae6607bf9c2954b5d21a851a
Sansan <msandartun@gmail.com>
2024-10-14 21:53:18 +0800

    cors

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const mailjet = Mailjet.apiConnect(
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors());
 const port = 3000;
 
 app.get("/blocked-dates", async (req, res) => {
